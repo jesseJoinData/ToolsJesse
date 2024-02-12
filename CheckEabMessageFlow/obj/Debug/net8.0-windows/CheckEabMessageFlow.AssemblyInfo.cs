44cab589fcb367f44ef0231bcc765f7383100bca
Jesse <jesse@join-data.nl>
2024-02-12 16:11:11 +0100

    all

--- CheckEabMessageFlow/obj/Debug/net8.0-windows/CheckEabMessageFlow.AssemblyInfo.cs
+++ CheckEabMessageFlow/obj/Debug/net8.0-windows/CheckEabMessageFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CheckEabMessageFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0c2b95705ea5baeda52568b54d3acc978e18c8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdcbd7e5dbd64dab455d5e28491c11932f60f6e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CheckEabMessageFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CheckEabMessageFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
